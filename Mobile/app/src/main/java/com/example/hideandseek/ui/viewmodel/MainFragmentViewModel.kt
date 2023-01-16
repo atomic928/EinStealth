c4e0daf396d6058cb522f0aeb3de8ccb7ccc4d5c
atomic928 <b1020055@fun.ac.jp>
2023-01-16 11:31:17 +0900

    罠に当たる範囲を修正

--- Mobile/app/src/main/java/com/example/hideandseek/ui/viewmodel/MainFragmentViewModel.kt
+++ Mobile/app/src/main/java/com/example/hideandseek/ui/viewmodel/MainFragmentViewModel.kt
@@ -89,7 +89,10 @@ class MainFragmentViewModel: ViewModel() {
     fun checkCaughtTrap(user: UserData, trap: TrapData): Boolean {
         // UserがTrapと一定の距離に来たかどうかを返す
         Log.d("checkCaughtTrap", (abs(user.latitude-trap.latitude) + abs(user.longitude-trap.longitude)).toString())
-        return abs(user.latitude-trap.latitude) < 0.0001 && abs(user.longitude-trap.longitude) < 0.0001
+        // 緯度・経度1どの違いで約100kmの差
+        // よって0.00001の差で1m程度の差になる
+        // 今回は0.000001以内、つまり10cm以内に入ったら当たった判定
+        return abs(user.latitude-trap.latitude) < 0.000001 && abs(user.longitude-trap.longitude) < 0.000001
     }
 
     fun howProgressSkillTime(relativeTime: String, skillTime: String): Int {
